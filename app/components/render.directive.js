6956e6dddbcdd3add25b694476df31aa4c4bcea8
Lorun <ruslan@lobarev.com>
2016-03-28 01:16:02 +0300

    remove old methods

--- app/components/render.directive.js
+++ app/components/render.directive.js
@@ -24,45 +24,6 @@
                 onRender(styles);
             }, true);
 
-            //console.log(buildService);
-
-
-
-            function buildCSS(buttons) {
-                var styles = '';
-
-                angular.forEach(buttons, function(button, key) {
-                    styles += buildOneElement(button);
-                });
-
-
-            }
-
-            function buildOneElement(button) {
-                var styles = '',
-                    className = button.role == 'common' ? button.classname : button.classname+':'+button.role;
-
-                styles += '.' + className + ' {\n';
-
-                if (button.role == 'common') {
-                    buildService.setCommon();
-                }
-
-                // Size
-                buildService.setSize(button.rules);
-
-
-
-                // Fill
-                if (button.rules.background) {
-                    styles += tab + 'background: ' + button.rules.background + ';\n';
-                }
-
-                styles += '}\n';
-
-                return styles;
-            }
-
             function onRender(styles) {
                 element.text(styles);
                 angular.element(document.getElementById('dynamic-css')).text(styles);
